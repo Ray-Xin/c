c86a42e40114ffc948271747fa4699866e3836bd
Ray-Xin <raymond.xin118@gmail.com>
2025-08-08 21:21:18 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,143 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        .............beebbbb............
+        ............eebbbb4bb...........
+        ............eb344bb4bb..........
+        ............e44334bb4bb.........
+        ............eb433344b4be........
+        ............4eb43344444be.......
+        ...........bd4eb43333344bb......
+        ..........b455d4443333444bb.....
+        ..........4d5555d444333444bb....
+        .........4555555dd4b4443444be...
+        ........bd5555d555d4bb444444ee..
+        ........b55ddd665555bb4b44444ee.
+        .......bd5555677655554ebb44444eb
+        .......43222558855555d4eeb44b4ee
+        ......b422332ddd555222d4eebbb4be
+        ......be22232ed55522332db4ebbbbe
+        .....bde22222e555e22232edd4bbbbe
+        .....b52e222e3555e22222eddd4ebee
+        ....bd552eee355552e222e355544eee
+        ....665dd5555555552eee355dd4deee
+        ...6776555555555555555551554d4ee
+        ...4885222555dddd6655551544d4eee
+        ..b45522332555dd677611d444ddeee.
+        ..4d5222232e55555881d44ddd4eee..
+        .bdd5e22222e555115114d54d4ee....
+        .b55d2e222e351144d1d55eeee......
+        bd5ddd2eee3d444555dd4e..........
+        b555115dddd55d544eede...........
+        4511d444d5544ee...4de...........
+        41d4555d4ee........44...........
+        41554eede.......................
+        44ee...4e.......................
+        `, mySprite, 50, 50)
+    projectile.follow(mySprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.gameOver(false)
+})
+let mySprite: Sprite = null
+let projectile: Sprite = null
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . b b d d d 5 5 5 5 5 5 5 b . . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    c b 5 5 b c d d 5 5 5 5 5 5 b . 
+    b b c c c d d d 5 5 5 5 5 d b . 
+    . . . . c c d d d 5 5 5 b b . . 
+    . . . . . . c c c c c b b . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite2.setPosition(8, 112)
+controller.moveSprite(mySprite2)
+scene.cameraFollowSprite(mySprite2)
+game.onUpdateInterval(100, function () {
+    mySprite = sprites.create(img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `, SpriteKind.Enemy)
+    info.changeScoreBy(1)
+    for (let index = 0; index < 100; index++) {
+        mySprite.setVelocity(randint(-50, 50), randint(-50, 50))
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            e e e . . . . e e e . . . . 
+            c d d c . . c d d c . . . . 
+            c b d d f f d d b c . . . . 
+            c 3 b d d b d b 3 c . . . . 
+            f b 3 d d d d 3 b f . . . . 
+            e d d d d d d d d e . . . . 
+            e d f d d d d f d e . b f b 
+            f d d f d d f d d f . f d f 
+            f b d d b b d d 2 f . f d f 
+            . f 2 2 2 2 2 2 b b f f d f 
+            . f b d d d d d d b b d b f 
+            . f d d d d d b d d f f f . 
+            . f d f f f d f f d f . . . 
+            . f f . . f f . . f f . . . 
+            `,img`
+            . . . . . . . . . . . . . . 
+            e e e . . . . e e e . . . . 
+            c d d c . . c d d c . . . . 
+            c b d d f f d d b c . . . . 
+            c 3 b d d b d b 3 c . . . . 
+            f b 3 d d d d 3 b f . . . . 
+            e d d d d d d d d e . . . . 
+            e d f d d d d f d e b f b . 
+            f d d f d d f d d f f d f . 
+            f b d d b b d d 2 b f d f . 
+            . f 2 2 2 2 2 2 d b d b f . 
+            . f d d d d d d d f f f . . 
+            . f d b d f f f d f . . . . 
+            . . f f f f . . f f . . . . 
+            `,img`
+            . . . . . . . . . . . . . . 
+            e e e . . . . e e e . . . . 
+            c d d c . . c d d c . . . . 
+            c b d d f f d d b c . . . . 
+            c 3 b d d b d b 3 c . . . . 
+            f b 3 d d d d 3 b f . . . . 
+            e d d d d d d d d e . . . . 
+            e d f d d d d f d e . b f b 
+            f d d f d d f d d f . f d f 
+            f b d d b b d d 2 b f f d f 
+            . f 2 2 2 2 2 2 d b b d b f 
+            . f d d d d d d d f f f f . 
+            . . f d b d f d f . . . . . 
+            . . . f f f f f f . . . . . 
+            `],
+        100,
+        true
+        )
+    }
+})
